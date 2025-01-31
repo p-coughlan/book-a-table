eb9a837a8d4c8bc31897018be17c86c6503f9510
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-01-31 12:27:55 +0000

    Add booking form and view for table reservations

---
+++ bookings/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['name', 'email', 'phone', 'date', 'time', 'guests']
+
+    widgets = {
+        'date': forms.DateInput(attrs={'type': 'date'}),
+        'time': forms.TimeInput(attrs={'type': 'time'}),
+    }
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookingForm
 
 # Create your views here.
+def book_table(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_success')
+    else:
+        form = BookingForm()
+
+    return render(request, 'bookings/book_table.html', {'form': form})
