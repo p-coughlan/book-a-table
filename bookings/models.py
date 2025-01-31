d073cc6d5f28406454e9b537dbc81d89eda2c7bb
Patrick Coughlan <patrickcoughlan@yahoo.co.uk>
2025-01-31 11:11:10 +0000

    Add Booking model and initial migration for restaurant booking app

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.date} at {self.time}"
